aedeaaf10842c61655a5e43cb8dcbac3b1d1f6eb
Victor Vicente <vitor@bu.edu>
2023-06-07 14:47:03 +0100

    Ore Generation

--- src/main/java/com/grandmen123/xtutorial/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/grandmen123/xtutorial/worldgen/ModPlacedFeatures.java
@@ -10,7 +10,9 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
@@ -18,12 +20,35 @@ import java.util.List;
 
 public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> EBONY_PLACED_KEY = createKey("ebony_placed");
+    public static final ResourceKey<PlacedFeature> BLACK_OPAL_PLACED_KEY = createKey("black_opal_placed");
+    public static final ResourceKey<PlacedFeature> NETHER_BLACK_OPAL_PLACED_KEY = createKey("nether_black_opal_placed");
+    public static final ResourceKey<PlacedFeature> END_BLACK_OPAL_PLACED_KEY = createKey("end_black_opal_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
         register(context, EBONY_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.EBONY_KEY),
                  VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
                                                     ModBlocks.EBONY_SAPLING.get()));
+
+        register(context, BLACK_OPAL_PLACED_KEY,
+                 configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_BLACK_OPAL_ORE_KEY),
+                 ModOrePlacement.commonOrePlacement(16,
+                                                    HeightRangePlacement.uniform(VerticalAnchor.absolute(-64),
+                                                                                 VerticalAnchor.absolute(64))
+                 ));
+        register(context, NETHER_BLACK_OPAL_PLACED_KEY,
+                 configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_BLACK_OPAL_ORE_KEY),
+                 ModOrePlacement.commonOrePlacement(16,
+                                                    HeightRangePlacement.uniform(VerticalAnchor.absolute(-64),
+                                                                                 VerticalAnchor.absolute(100))
+                 ));
+        register(context, END_BLACK_OPAL_PLACED_KEY,
+                 configuredFeatures.getOrThrow(ModConfiguredFeatures.END_BLACK_OPAL_ORE_KEY),
+                 ModOrePlacement.commonOrePlacement(16,
+                                                    HeightRangePlacement.uniform(VerticalAnchor.absolute(-64),
+                                                                                 VerticalAnchor.absolute(64))
+                 ));
     }
 
     private static ResourceKey<PlacedFeature> createKey(String name) {
